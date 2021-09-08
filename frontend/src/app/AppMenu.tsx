28a6801246e23a32b353ad5e87e0516f1cbb013e
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2021-09-08 17:56:57 +0400

    WIP: pet crud

--- frontend/src/app/AppMenu.tsx
+++ frontend/src/app/AppMenu.tsx
@@ -25,6 +25,18 @@ export const AppMenu = (props: AppMenuProps) => {
         caption={"screen.OwnerEditor"}
         key={"2797bc65-abbe-464a-8cb6-554d56fa1e7e"}
       />
+      <MenuItem
+        screenId={"PetList"}
+        icon={<BarsOutlined />}
+        caption={"screen.PetList"}
+        key={"74e70596-e754-4a6c-8a93-09c36714a632"}
+      />
+      <MenuItem
+        screenId={"PetEditor"}
+        icon={<BarsOutlined />}
+        caption={"screen.PetEditor"}
+        key={"d3e8e635-eb59-42ab-aac8-b6d1e3190f70"}
+      />
     </VerticalMenu>
   );
 };
--- frontend/src/routing.ts
+++ frontend/src/routing.ts
@@ -1,3 +1,5 @@
+import "./app/petEditor/PetEditor";
+import "./app/pet-list/PetList";
 import "./app/owner-editor/OwnerEditor";
 import "./app/owner-list/OwnerList";
 import { menuItems } from "@haulmont/jmix-react-ui";
