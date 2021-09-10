e8203a92abcabebd367a34fce3bb073aabad354a
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2021-09-10 18:27:17 +0400

    WIP: remove pet & read-only screens

--- frontend/src/app/AppMenu.tsx
+++ frontend/src/app/AppMenu.tsx
@@ -1,6 +1,5 @@
 import { MenuProps } from "antd";
 import { HomeOutlined, BarsOutlined } from "@ant-design/icons";
-import { MenuItem, VerticalMenu } from "@haulmont/jmix-react-ui";
 
 export interface AppMenuProps extends MenuProps {}
 
@@ -24,36 +23,6 @@ export const AppMenu = (props: AppMenuProps) => {
         icon={<BarsOutlined />}
         caption={"screen.OwnerEditor"}
         key={"3f2019b4-6945-4591-a8f1-a3521e64023a"}
-      />
-      <MenuItem
-        screenId={"PetList"}
-        icon={<BarsOutlined />}
-        caption={"screen.PetList"}
-        key={"0423b6c5-d8d3-4502-a23f-b660f28bb582"}
-      />
-      <MenuItem
-        screenId={"PetEditor"}
-        icon={<BarsOutlined />}
-        caption={"screen.PetEditor"}
-        key={"51c17ae2-5fa5-44e5-808f-ec25d067434b"}
-      />
-      <MenuItem
-        screenId={"ReadOnlyOwnerList"}
-        icon={<BarsOutlined />}
-        caption={"screen.ReadOnlyOwnerList"}
-        key={"445ec3f8-74c8-4896-9284-26917b9a8782"}
-      />
-      <MenuItem
-        screenId={"ReadOnlyOwnerListWithDetails"}
-        icon={<BarsOutlined />}
-        caption={"screen.ReadOnlyOwnerListWithDetails"}
-        key={"5d8dc8a2-b1b9-4cef-9182-7da0e1d92770"}
-      />
-      <MenuItem
-        screenId={"ReadOnlyOwnerDetails"}
-        icon={<BarsOutlined />}
-        caption={"screen.ReadOnlyOwnerDetails"}
-        key={"aa7cf5b6-6dc6-47ef-9f95-d97cf54740eb"}
       />
     </VerticalMenu>
   );
--- frontend/src/routing.ts
+++
@@ -1,9 +0,0 @@
-import "./app/read-only-owner-details/ReadOnlyOwnerDetails";
-import "./app/read-only-owner-list-with-details/ReadOnlyOwnerListWithDetails";
-import "./app/read-only-owner-list/ReadOnlyOwnerList";
-import "./app/petEditor/PetEditor";
-import "./app/pet-list/PetList";
-import "./app/owner-editor/OwnerEditor";
-import "./app/owner-list/OwnerList";
-import { menuItems } from "@haulmont/jmix-react-ui";
-import "./app/home/Home";
