d36603a6b0000fea5ac3ed61103df9d3501c52da
Gleb Durygin <durygin@haulmont.com>
2021-10-04 23:54:42 +0400

    Add username property to OwnerDTO and OwnerInputDTO

--- src/main/java/io/jmix2mvp/petclinic/dto/OwnerDTO.java
+++ src/main/java/io/jmix2mvp/petclinic/dto/OwnerDTO.java
@@ -7,6 +7,7 @@ public class OwnerDTO extends BaseDTO {
     private String city;
     private String email;
     private String telephone;
+    private String username;
 
     public String getFirstName() {
         return firstName;
@@ -54,5 +55,13 @@ public class OwnerDTO extends BaseDTO {
 
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
