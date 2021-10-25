b999e36dbc6635595e13d34af51cd6951735858c
Roman Karandashov <r.karandashov@haulmont.com>
2021-10-25 10:54:40 +0400

    i18n-params

--- frontend/src/app/owner-editor/OwnerEditor.tsx
+++ frontend/src/app/owner-editor/OwnerEditor.tsx
@@ -7,10 +7,11 @@ import {
   ApolloError,
   ApolloCache
 } from "@apollo/client";
+import dayjs from 'dayjs';
 import { Form, Button, Card, message, Alert, Spin, Result, Input } from "antd";
 import { useForm } from "antd/es/form/Form";
 import { observer } from "mobx-react";
-import { FormattedMessage, useIntl } from "react-intl";
+import { FormattedMessage, FormattedNumber, useIntl } from "react-intl";
 import { useHistory } from "react-router-dom";
 import { EntityDetailsScreenProps, useScreens } from "@amplicode/react-core";
 
@@ -43,6 +44,7 @@ const UPDATE__OWNER = gql`
 `;
 
 const OwnerEditor = observer(({ id }: EntityDetailsScreenProps) => {
+  const [state, setStatue] = useState<{ someProp: string }>({ someProp: "some custom property of custom object" });
   const [form] = useForm();
   const intl = useIntl();
   const screens = useScreens();
@@ -134,6 +136,172 @@ const OwnerEditor = observer(({ id }: EntityDetailsScreenProps) => {
 
   return (
     <Card className="narrow-layout">
+      <div>
+        <FormattedMessage // we don't have information on front-side about user at moment
+          id={"userLogin"}
+          values={{
+            userLogin: "unable"
+          }}
+        />
+      </div>
+      
+      <div> 
+        <FormattedMessage // you can see all dayjs formats on this link https://day.js.org/docs/en/display/format
+          id={"currentDate"}
+          values={{
+            currentDate: dayjs().format("YYYY-M-D"),
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"currentTime"}
+          values={{
+            currentTime: dayjs().format("HH:mm:ss"),
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"currentDateTime"}
+          values={{
+            currentDateTime: dayjs().format("YYYY-M-D HH:mm:ss"),
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"firstYearDay"}
+          values={{
+            firstYearDay: dayjs().startOf("year").format("YYYY-M-D")
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"lastYearDay"}
+          values={{
+            lastYearDay: dayjs().endOf("year").format("YYYY-M-D")
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"firstMonthDay"}
+          values={{
+            firstMonthDay: dayjs().startOf("month").format("YYYY-M-D")
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"lastMonthDay"}
+          values={{
+            lastMonthDay: dayjs().endOf("month").format("YYYY-M-D"),
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"firstWeekDay"}
+          values={{
+            firstWeekDay: dayjs().startOf("week").format("YYYY-M-D")
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"lastWeekDay"}
+          values={{
+            lastWeekDay: dayjs().endOf("week").format("YYYY-M-D")
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"startPreviousDay"}
+          values={{
+            startPreviousDay: dayjs().subtract(1, "day").startOf("day").format("YYYY-M-D HH:mm:ss")
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"startNextDay"}
+          values={{
+            startNextDay: dayjs().add(1, "day").startOf("day").format("YYYY-M-D HH:mm:ss"),
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"startCurrentHour"}
+          values={{
+            startCurrentHour: dayjs().startOf("hour").format("HH:mm:ss"),
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"endCurrentHour"}
+          values={{
+            endCurrentHour: dayjs().endOf("hour").format("HH:mm:ss"),
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"startCurrentMinute"}
+          values={{
+            startCurrentMinute: dayjs().startOf("minute").format("HH:mm:ss"),
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"endCurrentMinute"}
+          values={{
+            endCurrentMinute: dayjs().endOf("minute").format("HH:mm:ss")
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"testedNumber"}
+          values={{
+            testedNumber: <FormattedNumber value={45412.564} maximumFractionDigits={2} />
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"testedEntityProp"}
+          values={{
+            testedEntityProp: item?.city ?? "non value",
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage
+          id={"testedSomeComponentVariable"}
+          values={{
+            testedSomeComponentVariable: state.someProp
+          }}
+        />
+      </div>
+      <div>
+        <FormattedMessage // you can see all propertes of FormattedNumber on this link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#parameters
+          id={"allTypesKey"}
+          values={{
+            testedEntityProp: item?.city ?? "non value",
+            currentDateTime: dayjs().format("YYYY"),
+            testedNumber: <FormattedNumber value={45412.564} maximumFractionDigits={2} /> // maximumFractionDigits - the maximum number of fraction digits to use
+          }}
+        />
+      </div>
+
+
+
       <Form
         onFinish={handleSubmit}
         onFinishFailed={handleSubmitFailed}
